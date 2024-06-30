d7d9fb71e0f44f7fecbef67dbd1af24902f085a6
Gabriel Fonseca <biellimaet@gmail.com>
2024-06-30 13:45:38 -0300

    Added news annotations

--- hello/hello.go
+++ hello/hello.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+//Temos como declarar uma CONST dentro do GOLANG, não sendo possivel ser alterada.
+
 func main() {
 	/*Há algumas maneiras de você declarar variáveis em GO e uma delas é utilizando a tipagem juntamente ao VAR;
 	Ex: var nome string = "Meu nome é Gabriel" - Essa é a maneira mais explícita de declaração, sendo necessário declarar
@@ -59,8 +61,8 @@ func showIntroduce() {
 	name, _ := showNamesAndAge() // A utilização do "_" serve para declarar uma variável que não tenho interesse em usar em funções de múltiplos valores.
 	version := 1.0
 
-	fmt.Println("Hello Word, sr", name)
-	fmt.Println("The version program", version)
+	fmt.Println("Olá, sr", name)
+	fmt.Println("\nThe version program", version)
 }
 
 // No GOLANG você declara o retorno de uma função depois dos parâmetros.
@@ -73,11 +75,11 @@ func readCommand() int {
 }
 
 func showMenu() {
-
+	readFileSite()
 	fmt.Println("------------- / ------------- /")
-	fmt.Println("1 - Initialize monitoring")
-	fmt.Println("2 - Show logs")
-	fmt.Println("3 - Exit programm")
+	fmt.Println("1 - Inicializando monitoramento")
+	fmt.Println("2 - Visualizar logs")
+	fmt.Println("3 - sair do programa")
 
 }
 
@@ -89,14 +91,23 @@ func exitProgramm() {
 func initializeMonitoring() {
 	fmt.Println("Monitoring...")
 
-	site := "https://www.alura.com.br"
-	response, _ := http.Get(site)
+	sites := []string{"https://www.alura.com.br", "https://random-status-code.herokuapp.com/", "https://www.caelum.com.br"}
 
-	if response.StatusCode == 200 {
-		fmt.Println("O site ", site, "está online com sucesso!")
-	} else {
-		fmt.Println("O site ", site, "está com problema!")
+	//Você pode declarar o FOR assim:
+	// for i := 0; i < len(sites); i++ {
+	// 	response, _ := http.Get(sites[i])
+
+	// 	if response.StatusCode == 200 {
+	// 		fmt.Println("O site ", sites[i], "está online com sucesso!")
+	// 	} else {
+	// 		fmt.Println("O site ", sites[i], "está com problema!")
+	// 	}
+	// }
+	// Ou trabalhando com Range, assim:
+	for _, site := range sites {
+		checkSite(site)
 	}
+
 }
 
 // Para função de múltiplos retornos, você deve declarar os 2 tipos, exemplo:
@@ -125,3 +136,27 @@ Em resumo a função cap em Go retorna a capacidade atual do slice, que é o nú
 acomodar sem alocar mais memória. Quando você usa a função append e a capacidade atual do slice é excedida, Go aloca mais memória para o slice.
 O interessante aqui é que Go não apenas aloca memória para o novo elemento, mas também adiciona algum espaço extra para acomodar futuros elementos,
 para evitar a alocação de memória a cada chamada de append. Isso é feito para melhorar o desempenho.*/
+
+func checkSite(site string) {
+	response, _ := http.Get(site)
+
+	if response.StatusCode == 200 {
+		fmt.Println("O site ", site, "está online com sucesso!")
+	} else {
+		fmt.Println("O site ", site, "está com problema!")
+	}
+}
+
+func readFileSite() []string {
+	var sites []string
+	files, err := os.Open("site.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro ", err)
+	}
+	fmt.Println(files)
+
+	return sites
+}
+
+//NIL em GOLANG significa o equivalente a NULO
